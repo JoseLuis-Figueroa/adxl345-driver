00515f3a031db41ef81ae8d66da488cf570ae243
Jose Luis Figueroa de Lara <figueroadelara@outlook.com>
2025-03-19 14:18:49 -0600

    Add: Add DIO_configSizeGet function.
    The DIO_configSizeGet function was added to get the table size automatically.
    
    The comments were updated according to the updates.

--- adxl345_code/src/dio_cfg.c
+++ adxl345_code/src/dio_cfg.c
@@ -3,10 +3,10 @@
  * @author Jose Luis Figueroa
  * @brief This module contains the implementation for the digital 
  * input/output peripheral configuration.
- * @version 1.0
- * @date 2023-03-17
+ * @version 1.1
+ * @date 2025-03-19
  * 
- * @copyright Copyright (c) 2023 Jose Luis Figueroa. MIT License.
+ * @copyright Copyright (c) 2025 Jose Luis Figueroa. MIT License.
  * 
  */
 
@@ -35,8 +35,7 @@
  * input/output peripheral channel (pin). Each row represent a single pin.
  * Each column is representing a member of the DioConfig_t structure. This 
  * table is read in by Dio_Init, where each channel is then set up based on 
- * this table. The NUMBER_DIGITAL_PINS constant should be accorded with the
- * number of rows.
+ * this table. 
 */
 const DioConfig_t DioConfig[] = 
 {
@@ -65,18 +64,23 @@ const DioConfig_t DioConfig[] =
  * This function is used to initialize the DIO based on the configuration
  * table defined in dio_cfg module.
  * 
- * PRE-CONDITION: configuration table needs to be populated (sizeof > 0)
+ * PRE-CONDITION: configuration table needs to be populated (sizeof > 0) <br>
+ * 
  * POST-CONDITION: A constant pointer to the first member of the  
- * configuration table will be returned.
- * @return A pointer to the configuration table.
+ * configuration table will be returned.<br>
+ * 
+ * @return A pointer to the configuration table. <br>
  * 
  * \b Example: 
  * @code
- * const Dio_ConfigType_t * const DioConfig = DIO_configGet();
+ * const Dio_Config_t * const DioConfig = DIO_configGet();
+ * size_t configSize = DIO_configSizeGet();
  * 
- * DIO_Init(DioConfig);
+ * DIO_Init(DioConfig, configSize);
  * @endcode
  * 
+ * @see DIO_configGet
+ * @see DIO_configSizeGet
  * @see DIO_init
  * @see DIO_channelRead
  * @see DIO_channelWrite
@@ -93,4 +97,40 @@ const DioConfig_t * const DIO_configGet(void)
    */
   return (const DioConfig_t*)&DioConfig[0];
 
+}
+
+/*****************************************************************************
+ * Function: DIO_getConfigSize()
+*/
+/**
+*\b Description:
+ * This function is used to get the size of the configuration table.
+ * 
+ * PRE-CONDITION: configuration table needs to be populated (sizeof > 0) <br>
+ * 
+ * POST-CONDITION: The size of the configuration table will be returned. <br>
+ * 
+ * @return The size of the configuration table.
+ * 
+ * \b Example: 
+ * @code
+ * const Dio_Config_t * const DioConfig = DIO_configGet();
+ * size_t configSize = DIO_configSizeGet();
+ * 
+ * DIO_Init(DioConfig, configSize);
+ * @endcode
+ * 
+ * @see DIO_configGet
+ * @see DIO_configSizeGet
+ * @see DIO_init
+ * @see DIO_channelRead
+ * @see DIO_channelWrite
+ * @see DIO_channelToggle
+ * @see DIO_registerWrite
+ * @see DIO_registerRead
+ * 
+*****************************************************************************/
+size_t DIO_configSizeGet(void)
+{
+   return sizeof(DioConfig)/sizeof(DioConfig[0]);
 }

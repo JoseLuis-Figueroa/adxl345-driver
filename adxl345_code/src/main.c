08c1c853dae34fcbd8e7b230dec6d6d1704428b2
Jose Luis Figueroa de Lara <figueroadelara@outlook.com>
2025-03-21 13:42:14 -0600

    Update: Changes the main code.
    Correct function references and enhance ADXL345, DIO, and SPI interfaces with new configuration struct.

--- adxl345_code/src/main.c
+++ adxl345_code/src/main.c
@@ -2,12 +2,12 @@
  * @file main.c
  * @author Jose Luis Figueroa
  * @brief Implement the adxl345 accelerometer using Nucleo-F401RE. 
- * @version 1.0
- * @date 2023-10-13
+ * @version 1.1
+ * @date 2025-03-21
  * @note The debug mode is used to test and review the behavior of the
  * ADXL345 accelerometer.
  * 
- * @copyright Copyright (c) 2023 Jose Luis Figueroa. MIT License.
+ * @copyright Copyright (c) 2025 Jose Luis Figueroa. MIT License.
  * 
 */
 
@@ -31,21 +31,36 @@ int main (void)
 
     /*Get the address of the configuration table for DIO*/
     const DioConfig_t * const DioConfig = DIO_configGet();
-    /*Initialize the DIO pins according to the configuration table*/
-    DIO_init(DioConfig);
+    /*Get the size of the configuration table*/
+    size_t configSizeDio = DIO_configSizeGet();
+    /* Initialize the DIO pins according to the configuration table*/
+    DIO_init(DioConfig, configSizeDio);
 
     /*Get the address of the configuration table for SPI*/
     const SpiConfig_t * const SpiConfig = SPI_ConfigGet();
+    /*Get the size of the configuration table*/
+    size_t configSizeSpi = SPI_configSizeGet();
     /*Initialize the SPI channel according to the configuration table*/
-    SPI_init(SpiConfig);
+    SPI_init(SpiConfig, configSizeSpi);
+
+
+    /*ADXL345 configuration data*/
+    Adxl345Config_t Adxl345Config =
+    {
+        .Channel = SPI_CHANNEL1,
+        .Port = DIO_PA,
+        
+        .Pin = DIO_PA4
+    };
 
     /*Initialize accelerometer*/
-    ADXL345_init(SPI_CHANNEL1, DIO_PA, DIO_PA4);
+    ADXL345_init(&Adxl345Config);
 
     while(1)
     {
         /*Read the axis values*/
-        ADXL345_read(SPI_CHANNEL1, DIO_PA, DIO_PA4, DATA_START_R, dataAxis);
+        ADXL345_read(&Adxl345Config, DATA_START_R, 
+        sizeof(dataAxis)/sizeof(dataAxis[0]), &dataAxis[0]);
 
         /*Get x, y, z. Order the bytes (x0 and x1) on one 16 bits variable*/
         x = ((dataAxis[1]<<8) | dataAxis[0]);
